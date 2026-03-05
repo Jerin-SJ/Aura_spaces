69e9d5bdc154bcb32caed5981bdef733efe4ade8
Jerin-SJ <69037017+Jerin-SJ@users.noreply.github.com>
2026-03-05 22:43:17 +0000

    Last upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/Aura-spaces/', 
+    base: '/Aura_spaces/', 
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
